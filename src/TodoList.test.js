903b3413addf050ac9283b93f3333a0dccc9762c
LordNtlaloe <ntlal0e182@gmail.com>
2024-07-31 14:07:20 +0200

    Added TodoList.test.js and a mock REST API call

---
+++ src/TodoList.test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+// import '@testing-library/jest-dom'
+import {render} from '@testing-library/react'
+import App from './App'; 
+
+describe('<App /> tests', () => {
+  it('renders <App />', () => {
+    render(<App />)
+  })
+})
+
