56cf2721b1e1b8d82ac82b2bace091ed9810a376
LordNtlaloe <ntlal0e182@gmail.com>
2024-08-01 19:25:23 +0200

    Add A Failing Test For TodoList

---
+++ src/components/TodoItem/TodoItem.test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react';
+import TodoItem from "./TodoItem";
+import mockData from '../../mockData';
+
+describe('<TodoItem /> tests', () => {
+    it('should render todo item properly', () => {
+        render(<TodoItem todo={mockData[0]} />)
+        expect(screen.queryByText(/eat breakfast/i)).toBeInTheDocument();
+        expect(screen.getByTestId('close-btn-1')).toBeInTheDocument();
+    })
+})
