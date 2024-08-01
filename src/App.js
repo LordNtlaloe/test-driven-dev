c34031abddcd54fdaa61bd8c127cef778d614d6e
LordNtlaloe <ntlal0e182@gmail.com>
2024-08-01 19:12:31 +0200

    Added A Save ToDo Button and Form

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
   
   const [ todos, setTodos ] = useState([])
   const [loading, setLoading] = useState(true)
+  const [newTodo, setNewTodo] = useState('')
+  const [saving, setSaving] = useState(false)
   useEffect(() => {
     async function fetchData() {
       const result = await fetch('https://jsonplaceholder.typicode.com/todos').then((response) => 
@@ -16,10 +18,46 @@ function App() {
     }
     fetchData();
   }, [])
+
+  function onChange(e){
+    const value = e.target.value;
+    setNewTodo(value);
+  }
+
+  function addTodo(e){
+    e.preventDefault();
+    const value = {
+      userId: 3,
+      id: Math.floor(Math.random() * 10000) + 1,
+      title: newTodo,
+      completed: false
+    };
+    
+    setSaving(true);
+    fetch('https://jsonplaceholder.typicode.com/todos', {
+      method: 'POST',
+      body: JSON.stringify(value),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      }
+    }).then((response) => response.json())
+      .then((result) => {
+        setTodos(todos.concat({...result, id: value.id}))
+        setSaving(false)
+      })
+  }
   return (
     <div className="App">
       <h1 className='header'>The Ultimate To Do List</h1>
       {loading ? 'Loading' : <TodoList todos={todos} />}
+      <div className="add-todo-form">
+        {saving ? ('Saving') : (
+          <form onSubmit={addTodo}>
+            <input type="text" onChange={onChange}/>
+            <button type='submit'>Add New Todo</button>
+          </form>
+        )}
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
 import App from './App';
-import mockData from './mockData';
-import fetchMock from 'jest-fetch-mock';
+import userEvent from '@testing-library/user-event';
 
 
-beforeEach(() => {
-  fetchMock.once(JSON.stringify(mockData));
-});
 
 describe('<App /> tests', () => {
   it('renders <App />', async () => {
-    render(<App />);
-    await waitForElementToBeRemoved(() => screen.getByText(/loading/i));
+    fetchMock.once(JSON.stringify({
+      userId: 3,
+      id: Math.floor(Math.random() * 100) + 1,
+      title: 'Do Math Homework',
+      completed: false
+    })
+  )
+  render(<App />);
+  await waitForElementToBeRemoved(() => screen.getByText(/loading/i));
+  userEvent.type(screen.getByRole('textbox'), 'Do Math Homework');
+  userEvent.click(screen.getByText(/Add new todo/i));
+  await waitForElementToBeRemoved(() => screen.getByText(/saving/i))
+  except(screen.getByText(/Do math Homework/i)).toBeInTheDocument();
   });
+  
 });
